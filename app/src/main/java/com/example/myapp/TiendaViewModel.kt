187b8cc785eb935249c7055161d88a56dacee011
AlvaniHer <99564929+AlvaniHer@users.noreply.github.com>
2026-02-12 21:53:49 +0100

    Mas o menos bien(primer commit)

---
+++ app/src/main/java/com/example/myapp/TiendaViewModel.kt
@@ -0,0 +1,123 @@
+package com.example.myapp
+
+import androidx.lifecycle.ViewModel
+import com.example.myapp.data.Producto
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+/**
+ * Clase que actúa como el "cerebro" o administrador de datos de la tienda VaniShoots.
+ * * Su función principal es preparar y proteger la lista de servicios (fotos, vídeos, clips)
+ * para que las pantallas puedan leerlos sin riesgo de que los datos se pierdan o se dañen
+ * al girar el móvil o cambiar de sección.
+ */
+class TiendaViewModel : ViewModel() {
+   /**
+    * Lista interna y privada de los productos.
+    * Se usa [MutableStateFlow] para que la lista pueda actualizarse si fuera necesario.
+    * El guion bajo (_) indica que es una variable "secreta" que solo este archivo puede modificar.
+    */
+   private val _emailError = MutableStateFlow(false)
+    val emailError = _emailError.asStateFlow()
+    fun validaremail(email: String){
+        //si el email no tiene @ no es un corre
+        _emailError.value = !email.contains("@")
+
+    }
+    fun registrarUsuario(
+        auth: FirebaseAuth,
+        email: String,
+        password: String,
+        confPassword: String,
+        onSuccess: () -> Unit,
+        onFailure: (String) -> Unit
+    ) {
+        // Validamos primero: ¿Son iguales las contraseñas?
+        if (password != confPassword) {
+            onFailure("Las contraseñas no coinciden")
+            return
+        }
+        // Validamos: ¿Es muy corta?
+        if (password.length < 6) {
+            onFailure("La contraseña debe tener al menos 6 caracteres")
+            return
+        }
+        //si esta bien
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    onSuccess()
+                } else {
+                    onFailure(task.exception?.message ?: "Error desconocido")
+                }
+            }
+    }
+    // Lista de productos (Lógica de negocio)
+    private val _productos = MutableStateFlow(
+        listOf(
+            //fotos
+            Producto(
+                id = 0,
+                nombre = "Sesión de Fotos",
+                precio = "20€",
+                carruselFotos = listOf(
+                    R.drawable.f1,
+                    R.drawable.f2,
+                    R.drawable.f3,
+                    R.drawable.f4
+                ), //lista fotos
+                descripcion = "Sesion fotografica profesional, en alta resolucion, ideal para eventos o retratos personales. Pack:20 fotos(editadas)."
+            ),
+            // Videos
+            Producto(
+                id = 1,
+                nombre = "Muestra de Videos",
+                precio = "50€",
+                carruselVideos = listOf(
+                    R.drawable.v1,
+                    R.drawable.v2,
+                    R.drawable.v3
+                ), // Lista videos
+                descripcion = "Toma y edicion de video cinematofrafico con correcion de color y montaje de audio profesional."
+            ),
+            //Clips
+            Producto(
+                id = 2,
+                nombre = "Clips para Redes",
+                precio = "20€",
+                carruselClips = listOf(
+                    R.drawable.c1,
+                    R.drawable.c2,
+                    R.drawable.c3,
+                    R.drawable.c4,
+                    R.drawable.c5
+                ), // Lista clips
+                descripcion = "Clips cortos optimizados para redes sociales (Reels, TikTok) con transiciones dinamicas. (30s max)."
+            ),
+        )
+    )
+    private val _carrito = MutableStateFlow<List<Producto>>(emptyList())
+    val carrito = _carrito.asStateFlow()
+    // Función para el botón Comprar
+    fun añadirAlCarrito(producto: Producto) {
+        _carrito.update { listaActual: List<Producto> ->
+            listaActual + producto
+        }
+    }
+    fun eliminarDelCarrito(productoId: Int) {
+        _carrito.value = _carrito.value.filter { it.id != productoId }
+    }
+    fun limpiarCarrito() {
+        _carrito.value = emptyList() //vaciar la StateFlow
+    }
+
+    /**
+     * Canal público de datos que observan las pantallas
+     * Es un [StateFlow] de solo lectura, lo que garantiza que la interfaz solo
+     * pueda "ver" los productos pero no borrarlos ni cambiarlos directamente.
+     */
+    val productos: StateFlow<List<Producto>> = _productos
+}
---
+++ app/src/main/java/com/example/myapp/data/Producto.kt
@@ -0,0 +1,27 @@
+package com.example.myapp.data
+
+/**
+ * Datos que representa un servicio o paquete de la tienda VaniShoots.
+ *
+ * Esta clase se encarga de agrupar toda la información que necesita un producto
+ * para mostrarse en la aplicación, desde sus textos básicos hasta las listas
+ * de imágenes para los carruseles visuales.
+ *
+ * @property id Número único que identifica al producto
+ * @property nombre El título del servicio
+ * @property precio El costo del servicio guardado como texto para facilitar su visualización.
+ * @property descripcion Texto detallado que explica qué incluye el paquete contratado.
+ * @property carruselFotos Lista de recursos  de imágenes para la galería principal.
+ * @property carruselClips Lista de recursos de imágenes para la sección de clips rápidos.
+ * @property carruselVideos Lista de recursos que sirven como miniaturas para los vídeos largos.
+ */
+data class Producto(
+    val id: Int,
+    val nombre: String,
+    val precio: String,
+    val descripcion: String,
+    // Lista de imágenes para que el carrusel funcione
+    val carruselFotos: List<Int> = emptyList(),  // Lista fotos
+    val carruselClips: List<Int> = emptyList(),  // lista clips
+    val carruselVideos: List<Int> = emptyList()  // lista videos (miniaturas)
+)
